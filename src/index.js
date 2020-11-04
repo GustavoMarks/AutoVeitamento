398d3044dc959d4b582d8c33a043ba230b9df29b
Gustavo Marques <gustavosmarquesf@gmail.com>
2020-11-04 19:57:40 -0300

    refactor: removendo arquivos de tamplate não utilizados, issue #10

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { createStore } from 'redux';
 import {Provider} from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import rootReducer from './store/reducers/rootReducer';
 
@@ -16,8 +15,3 @@ ReactDOM.render(
 		<App />
 	</Provider>
 	, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
